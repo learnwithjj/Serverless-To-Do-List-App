b3eb8ae90fb2e9d7dc0d92e6ac8dffd6e1516e03
Apple <apple@Sussegado-MacBook-Air.local>
2025-09-14 09:45:43 +0530

    changes

--- ToDoList-infra/modules/lambda/main.tf
+++ ToDoList-infra/modules/lambda/main.tf
@@ -8,7 +8,7 @@ resource "aws_lambda_function" "crud" {
   filename         = data.archive_file.lambda_function_crud.output_path
   function_name    = "${var.environment}-crudtodolist"
   role             = var.lambda_iam_role
-  handler          = "lambda_function.lambda_handler"
+  handler          = "crud.lambda_handler"
   source_code_hash = data.archive_file.lambda_function_crud.output_base64sha256
   runtime          = "python3.13"
   environment {
